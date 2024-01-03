59072dc90de70c020b87c85f7275b108269bd317
Wnikiel <wojtek@nikiel.org>
2024-01-03 12:24:19 +0100

    Add: DataLoader

---
+++ Documents/FoulDetection/run.py
@@ -0,0 +1,22 @@
+from DataLoader import DataLoader
+import logging
+import os
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
+
+project_path = Path('run.py').resolve().parent
+data_path = project_path / 'sampleData'
+
+try:
+    logging.info("Started!")
+
+    data_loader = DataLoader(
+        data_path,
+        os.path.dirname(os.path.realpath(__file__))
+    )
+    data_loader._load_data()
+
+    logging.info("Script completed successfully!")
+except Exception as e:
+    logging.error(f"An error occurred: {e}")
