6a8b6e44ce395b2b0f17c547598e46d24c6a446b
Wnikiel <wojciech.nikiel@nokia.com>
2025-06-11 10:57:41 +0200

    Clean up repo

--- normalize_data.py
+++ normalize_data.py
@@ -12,32 +12,25 @@ def normalize_position(df, width, height):
 
 
 def process_and_save_csv(input_path, output_path, width, height):
-    # Wczytaj dane z pliku CSV
     df = pd.read_csv(input_path, header=None)
 
-    # Normalizuj dane
     df_normalized = normalize_position(df, width, height)
 
-    # Zapisz dane do nowego pliku CSV
     df_normalized.to_csv(output_path, index=False, header=None)
 
 
 def main():
-    # Ścieżka do katalogu z plikami CSV
     directory_path = Path(__file__).resolve().parent
     directory_path = directory_path / 'trainData' / 'NoFoul'
 
-    # Wymiary obrazka
     image_width = 854
     image_height = 480
 
-    # Przetwórz każdy plik CSV w katalogu
     for filename in os.listdir(directory_path):
         if filename.endswith(".csv"):
             input_file = os.path.join(directory_path, filename)
             output_file = os.path.join(directory_path, f"normalized_{filename}")
 
-            # Przetwórz i zapisz plik CSV
             process_and_save_csv(input_file, output_file, image_width, image_height)
 
 if __name__ == "__main__":
--- trainModel.py
+++ trainModel.py
@@ -4,6 +4,7 @@ from Model.DataCsvReader import VideoDataset
 from tensorflow.keras import layers, models
 import numpy as np
 
+
 def create_tensorflow_model(input_shape):
     model = models.Sequential()
     model.add(layers.LSTM(50, input_shape=input_shape, return_sequences=True))
@@ -14,37 +15,33 @@ def create_tensorflow_model(input_shape):
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
 
+
 train_data_folder = Path('C:/Users/wnikiel/Documents/FOuldetectionProject/FoulDetection/trainData')
 video_dataset = VideoDataset(root_dir=train_data_folder, num_frames_per_sample=10)
 
-# Wczytanie danych bez użycia DataLoader
 data = video_dataset.load_data()
 
-all_data = []  # Lista przechowująca wszystkie dane (klatki + etykiety)
+all_data = []
 for record in data:
-    frames = record[0]  # Klatki, bez ostatniej etykiety
-    label = torch.tensor(record[-1], dtype=torch.double)  # Ostatnia etykieta
+    frames = record[0]
+    label = torch.tensor(record[-1], dtype=torch.double)
     all_data.append((frames, label))
 
-# Podziel dane na zbiór treningowy i testowy
+
 train_size = int(0.8 * len(all_data))
 test_size = len(all_data) - train_size
 train_data, test_data = torch.utils.data.random_split(all_data, [train_size, test_size])
 
-# Przykładowe użycie modelu TensorFlow
-input_shape = (10, 103)  # Przykładowy kształt wejściowy
+input_shape = (10, 103)
 tensorflow_model = create_tensorflow_model(input_shape)
 batch_size = 32
 
-# Nauka modelu TensorFlow
-x_train = np.array([item[0].numpy() for item in train_data])  # Konwersja z PyTorch na numpy array
-y_train = np.array([item[1].numpy() for item in train_data])  # Konwersja z PyTorch na numpy array
+x_train = np.array([item[0].numpy() for item in train_data])
+y_train = np.array([item[1].numpy() for item in train_data])
 tensorflow_model.fit(x_train, y_train, epochs=10, batch_size=batch_size, validation_split=0.2)
 
-# Zapisz przetrenowany model TensorFlow
 tensorflow_model.save('tensorflow_foul_pred.h5')
 
-# Testowanie modelu na danych testowych
 x_test = np.array([item[0].numpy() for item in test_data])
 y_test = np.array([item[1].numpy() for item in test_data])
 
